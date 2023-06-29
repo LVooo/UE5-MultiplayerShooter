ca3a8de2bf478bf4c3c6756ebc502558b09daf96
LVooo <61344766+LVooo@users.noreply.github.com>
2023-06-29 15:43:29 +1000

    new branch

--- Blaster/Source/Blaster/PlayerState/BlasterPlayerState.h
+++ Blaster/Source/Blaster/PlayerState/BlasterPlayerState.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "Blaster/BlasterTypes/Team.h"
 #include "BlasterPlayerState.generated.h"
 
 /**
@@ -36,4 +37,11 @@ private:
 
 	UPROPERTY(ReplicatedUsing=OnRep_Defeats)
 	int32 Defeats;
+
+	UPROPERTY(Replicated)
+	ETeam Team = ETeam::ET_NoTeam;
+
+public:
+	FORCEINLINE ETeam GetTeam() const { return Team; }
+	FORCEINLINE void SetTeam(ETeam TeamToSet) { Team = TeamToSet; }
 };
