0d739ee76dc76b1c9b3d241a75bfaa9a46b4880a
LVooo <61344766+LVooo@users.noreply.github.com>
2023-06-30 10:32:50 +1000

    test git

--- Blaster/Source/Blaster/BlasterTypes/Annoucement.h
+++ Blaster/Source/Blaster/BlasterTypes/Annoucement.h
@@ -6,7 +6,7 @@ namespace Announcement
 	const FString ThereIsNoWinner(TEXT("There is no winner."));
 	const FString YouAreTheWinner(TEXT("You are the winner!"));
 	const FString PlayersTiedForTheWin(TEXT("Players tied for the win:"));
-	const FString TeamsTiedForTheWin(TEXT("The Game ends in a tie"));
+	const FString TeamsTiedForTheWin(TEXT("The Game ends in a tie."));
 	const FString RedTeam(TEXT("Red team"));
 	const FString BlueTeam(TEXT("Blue team"));
 	const FString RedTeamWins(TEXT("Red team wins!"));
